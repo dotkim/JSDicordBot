7dcde4f98ff2cc733defbe52a427d6904dc89323
Kim Nerli <32514438+dotkim@users.noreply.github.com>
2018-10-15 23:31:07 +0200

    Copy paste from doc
    Set up the project with dependencies.
    Just copy pasted doc examples to test it.

---
+++ bot.js
@@ -0,0 +1,23 @@
+const Discord = require('discord.js');
+const dotenv = require('dotenv').config();
+
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log('I am ready!');
+  });
+  
+  // Create an event listener for messages
+  client.on('message', message => {
+    // If the message is "ping"
+    if (message.content === 'ping') {
+      // Send "pong" to the same channel
+      message.channel.send('pong');
+    }
+    if (message.content === 'what is my avatar') {
+        // Send the user's avatar URL
+        message.reply(message.author.avatarURL);
+      }
+  });
+
+client.login(process.env.TOKEN)
