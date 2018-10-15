b787a53103b549879f3f49f3b7334bfe4491cef0
Kim Nerli <32514438+dotkim@users.noreply.github.com>
2018-10-16 00:07:39 +0200

    More testing
    Will be removed once i have added the correct func.
    Fixed indentations.

--- bot.js
+++ bot.js
@@ -4,20 +4,23 @@ const dotenv = require('dotenv').config();
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    console.log('I am ready!');
-  });
-  
-  // Create an event listener for messages
-  client.on('message', message => {
-    // If the message is "ping"
-    if (message.content === 'ping') {
-      // Send "pong" to the same channel
-      message.channel.send('pong');
-    }
-    if (message.content === 'what is my avatar') {
-        // Send the user's avatar URL
-        message.reply(message.author.avatarURL);
-      }
-  });
+	console.log('I am ready!');
+});
+
+// Create an event listener for messages
+client.on('message', message => {
+	// If the message is "ping"
+	if (message.content === 'ping') {
+		// Send "pong" to the same channel
+		message.channel.send('pong');
+	}
+	if (message.content === 'what is my avatar') {
+		// Send the user's avatar URL
+		message.reply(message.author.avatarURL);
+	}
+	if (message.content === 'show ava') {
+		message.channel.send(client.user.displayAvatarURL)
+	}
+});
 
 client.login(process.env.TOKEN)
