647956ed6c8d4a631df4e931b62c764d66e17817
Kim Nerli <32514438+dotkim@users.noreply.github.com>
2018-10-20 01:33:34 +0200

    Added more functions
    Should have been done in its own branch, but wasnt.

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const dotenv = require('dotenv').config();
 const request = require('request');
+const format = require('util').format;
 
 const client = new Discord.Client();
 
@@ -12,18 +13,49 @@ client.on('ready', () => {
 client.on('message', message => {
 	if (message.content.includes('!admin') === true) {
 		if (message.author.id.includes(process.env.ADMIN) === true) {
-				const collector = new Discord.MessageCollector(message.channel, m => m.author.id === message .author.id, { time: 20000});
-				console.log(collector)
-				message.reply('Waiting for command')
-					.then(result => {
-						collector.on('collect', message => {
-							if (message.content == 'gProgress') {
-								request('https://www.wowprogress.com/guild/eu/argent-dawn/Salvation/json_rank', function(error, response, body) {
-									result.edit(body);
-								})
-							}
-						})
-					});
+			message.delete();
+			const collector = new Discord.MessageCollector(message.channel, m => m.author.id === message.author.id, { time: 20000 });
+			message.reply('Waiting for command')
+				.then(result => {
+					collector.on('collect', message => {
+						if (message.content == 'rank') {
+							message.delete();
+							request('https://www.wowprogress.com/guild/eu/argent-dawn/Salvation/json_rank', function (error, response, body) {
+								body = JSON.parse(body)
+								result.edit(format('__**Guild Ranking**__\n**Score:** __%d__\n**World Rank:** __%d__\n**Realm rank:** __%d__', body.score, body.world_rank, body.realm_rank));
+							});
+						}
+						if (message.content == 'logs') {
+							request('https://www.warcraftlogs.com:443/v1/reports/guild/Salvation/Argent-Dawn/EU?start=1538156088557&api_key=' + process.env.APIKEY, (error, response, body) => {
+								body = JSON.parse(body)
+								let embed = new Discord.RichEmbed()
+									.setTitle(format('**Logname: %s**', body[0].title))
+									.setAuthor(message.author.username, message.author.avatarURL)
+									.setColor('RANDOM')
+									.setURL('https://www.warcraftlogs.com/reports/' + body[0].id)
+									.setDescription(
+										request('https://www.warcraftlogs.com:443/v1/report/fights/' + body[0].id + '?translate=false&api_key=' + process.env.APIKEY, (error, response, body) => {
+											body = JSON.parse(body)
+											let bosses = ""
+											let i;
+											for (i in body.fights) {
+												let curFight = body.fights[i]
+												if (curFight.kill != undefined && curFight.boss != 0) {
+													bosses += format('**Fight name:** %s', curFight.name)
+													/* console.log(curFight.name)
+													console.log(curFight.kill)
+													console.log(curFight.difficulty) */
+												}
+											return bosses
+											}
+										})
+									);
+								result.edit({ embed });
+								message.delete();
+							});
+						}
+					})
+				});
 		}
 		else {
 			message.channel.send('Nay')
